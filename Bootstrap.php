de645a3cba9df01cff01388a349853df118130fa
Dennis Heinrich <der@dennis-heinri.ch>
2024-05-07 10:27:52 +0200

    Grundlegenes Tracking mit Bestellabschluss implementiert

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,20 +1,24 @@
 <?php declare(strict_types=1);
 /**
- * @package Plugin\plugin_test
+ * @package Plugin\dh_matomo_tracking
  * @author Dennis Heinrich
  */
 
-namespace Plugin\plugin_test;
+namespace Plugin\dh_matomo_tracking;
 
 use JTL\Events\Dispatcher;
 use JTL\Plugin\Bootstrapper;
+use JTL\Shop;
+use Plugin\dh_matomo_tracking\classes\ShopTracking;
 
 /**
  * Class Bootstrap
- * @package Plugin\plugin_test
+ * @package Plugin\dh_matomo_tracking
  */
 class Bootstrap extends Bootstrapper
 {
+    private ShopTracking $shopTracking;
+
     /**
      * Executed on each plugin call (e.g. on each page visit)
      * @param Dispatcher $dispatcher
@@ -23,5 +27,80 @@ class Bootstrap extends Bootstrapper
     public function boot(Dispatcher $dispatcher): void
     {
         parent::boot($dispatcher);
+        try {
+            if($this->runComposerAutoload() && Shop::isFrontend()) {
+                $matomoSiteId = (int)$this->getPlugin()->getConfig()->getValue("site_id");
+                $matomoInstanceUrl = (string)$this->getPlugin()->getConfig()->getValue("instance_url");
+                $matomoAuthenticationToken = (string)$this->getPlugin()->getConfig()->getValue("authentication_token");
+
+                if($matomoSiteId > 0 && strlen($matomoInstanceUrl) > 0 && strlen($matomoAuthenticationToken) > 0) {
+                    $this->shopTracking = (new ShopTracking())
+                        ->setInstanceUrl($matomoInstanceUrl)
+                        ->setAuthenticationToken($matomoAuthenticationToken)
+                        ->setSiteId($matomoSiteId)
+                        ->createTracker();
+                    
+                    $fullProtocolDomainAndPath = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+                    $this->shopTracking->setPageUrl($fullProtocolDomainAndPath);
+                    $this->shopTracking->setLanguageCode($_SESSION['currentLanguage']->iso639);
+                    
+                    $dispatcher->listen('shop.hook.'.HOOK_BESTELLABSCHLUSS_PAGE, function($args) {
+                        /** @var \JTL\Checkout\Bestellung */
+                        $order = $args['oBestellung'];
+                        $orderPositions = $order->Positionen;
+                        foreach($orderPositions as $orderPosition) {
+                            try {
+                                if(isset($orderPosition->Artikel)) {
+                                    $this->shopTracking->addCartItem($orderPosition->Artikel, $orderPosition->nAnzahl);
+                                } else {
+                                    switch($orderPosition->nPosTyp) {
+                                        case C_WARENKORBPOS_TYP_VERSANDPOS:
+                                            $shippingCosts = (float)($orderPosition->fPreis * ($orderPosition->fMwSt/10));
+                                            $shippingCosts *= $orderPosition->nAnzahl;
+                                            $this->shopTracking->addCartItemCustom("", "Versandkosten", $shippingCosts, $orderPosition->nAnzahl);
+                                            break;
+                                    }
+                                }
+                            } catch(\Exception $e) {
+                                Shop::Container()->getLogService()->error("Fehler beim Hinzufügen eines Artikels zum Matomo-Tracking: " . $e->getMessage());
+                            }
+                        }
+                        $this->shopTracking->addOrder($order);
+                    });
+
+                    $dispatcher->listen('shop.hook.'.HOOK_ARTIKEL_PAGE, function($args) {
+                        try {
+                            $this->shopTracking->setProductViewed($args['oArtikel']);
+                        } catch(\Exception $e) {
+                            Shop::Container()->getLogService()->error("Fehler beim Setzen des angezeigten Artikels im Matomo-Tracking: " . $e->getMessage());
+                        }
+                    });
+
+                    $dispatcher->listen('shop.hook.'.HOOK_SMARTY_OUTPUTFILTER, function($args) {
+                        try {
+                            $this->shopTracking->completeTracking();
+                        } catch(\Exception $e) {
+                            Shop::Container()->getLogService()->error("Fehler beim Abschließen des Trackings: " . $e->getMessage());
+                        }
+                    });
+                } else {
+                    Shop::Container()->getLogService()->error("Das Plugin dh_matomo_tracking wurde nicht vollständig konfiguriert.");
+                }
+            } else {
+                Shop::Container()->getLogService()->error("Das Plugin dh_matomo_tracking wurde nicht korrekt installiert, die Composer-Abhängigkeiten fehlen");
+            }
+        } catch(\Exception $e) {
+            Shop::Container()->getLogService()->error("Fehler beim Initialisieren des Matomo-Trackings: " . $e->getMessage());
+        }
+    }
+
+    private function runComposerAutoload(): bool
+    {
+        $autoloadFilePath = __DIR__ ."/vendor/autoload.php";
+        if(file_exists($autoloadFilePath)) {
+            require_once $autoloadFilePath;
+            return true;
+        }
+        return false;
     }
 }
---
+++ classes/ShopTracking.php
@@ -0,0 +1,185 @@
+<?php
+namespace Plugin\dh_matomo_tracking\classes;
+
+use JTL\Cart\Cart;
+use JTL\Catalog\Product\Artikel;
+use JTL\Checkout\Bestellung;
+use MatomoTracker;
+
+class ShopTracking {
+    private int $siteId;
+    private string $instanceUrl;
+    private string $authenticationToken;
+    private MatomoTracker $matomoTracker;
+    private string $pageUrl;
+    private string $languageCode;
+    private ?Artikel $productViewed = null;
+
+    /**
+     * Get the value of siteId
+     * @return int
+     */
+    public function getSiteId(): int {
+        return $this->siteId;
+    }
+
+    /**
+     * Set the value of siteId
+     * @param int $siteId
+     * @return self
+     */
+    public function setSiteId(int $siteId): self {
+        $this->siteId = $siteId;
+        return $this;
+    }
+
+    /**
+     * Get the value of instanceUrl
+     * @return string
+     */
+    public function getInstanceUrl(): string {
+        return $this->instanceUrl;
+    }
+
+    /**
+     * Set the value of instanceUrl
+     * @param string $instanceUrl
+     * @return self
+     */
+    public function setInstanceUrl(string $instanceUrl): self {
+        $this->instanceUrl = $instanceUrl;
+        return $this;
+    }
+
+    /**
+     * Get the value of authenticationToken
+     * @return string
+     */
+    public function getAuthenticationToken(): string {
+        return $this->authenticationToken;
+    }
+
+    /**
+     * Set the value of authenticationToken
+     * @param string $authenticationToken
+     * @return self
+     */
+    public function setAuthenticationToken(string $authenticationToken): self {
+        $this->authenticationToken = $authenticationToken;
+        return $this;
+    }
+
+    /**
+     * Get the value of matomoTracker
+     * @return MatomoTracker
+     */
+    public function getMatomoTracker(): MatomoTracker {
+        return $this->matomoTracker;
+    }
+
+    /**
+     * Set the value of matomoTracker
+     * @param MatomoTracker $matomoTracker
+     * @return self
+     */
+    public function setMatomoTracker(MatomoTracker $matomoTracker): self {
+        $this->matomoTracker = $matomoTracker;
+        return $this;
+    }
+
+    public function createTracker(): self {
+        $newTracker = new MatomoTracker($this->getSiteId(), $this->getInstanceUrl());
+        $newTracker->setTokenAuth($this->getAuthenticationToken());
+        $this->matomoTracker = $newTracker;
+        return $this;
+    }
+
+    public function addCartItem(Artikel $product, int $quantity = 0)
+    {
+        $this->addCartItemCustom($product->cArtNr, $product->cName, $product->Preise->fVKBrutto, $quantity);
+    }
+
+    public function addCartItemCustom(string $cArtNr, string $cName, float $fPreis = 0, int $quantity = 0)
+    {
+        $this->matomoTracker->addEcommerceItem($cArtNr, $cName, '', $fPreis, $quantity);
+    }
+
+    public function addOrder(Bestellung $order)
+    {
+        $this->matomoTracker->doTrackEcommerceOrder($order->cBestellNr, $order->fGesamtsumme, 0.0, 0.0, 0.0, 0.0);
+    }
+
+    public function completeTracking()
+    {
+        if($this->matomoTracker === null) {
+            return;
+        }
+        
+        if($this->getProductViewed() !== null) {
+            // Todo Add category details
+            $this->matomoTracker->setEcommerceView($this->getProductViewed()->cArtNr, $this->getProductViewed()->cName, '', $this->getProductViewed()->Preise->fVKNetto);
+        }
+
+        if($this->getPageUrl() !== null) {
+            $this->matomoTracker->doTrackPageView($this->getPageUrl());
+        }
+
+        if($this->getLanguageCode() !== null) {
+            $this->matomoTracker->setBrowserLanguage($this->getLanguageCode());
+        }
+    }
+
+    /**
+     * Get the value of pageUrl
+     * @return string
+     */
+    public function getPageUrl(): string {
+        return $this->pageUrl;
+    }
+
+    /**
+     * Set the value of pageUrl
+     * @param string $pageUrl
+     * @return self
+     */
+    public function setPageUrl(string $pageUrl): self {
+        $this->pageUrl = $pageUrl;
+        return $this;
+    }
+
+    /**
+     * Get the value of languageCode
+     * @return string
+     */
+    public function getLanguageCode(): string {
+        return $this->languageCode;
+    }
+
+    /**
+     * Set the value of languageCode
+     * @param string $languageCode
+     * @return self
+     */
+    public function setLanguageCode(string $languageCode): self {
+        $this->languageCode = $languageCode;
+        return $this;
+    }
+
+    /**
+     * Get the value of product
+     * @return Artikel
+     */
+    public function getProductViewed(): ?Artikel {
+        return $this->productViewed;
+    }
+
+    /**
+     * Set the value of product
+     * @param Artikel $product
+     * @return self
+     */
+    public function setProductViewed(?Artikel $product): self {
+        $this->productViewed = $product;
+        return $this;
+    }
+}
